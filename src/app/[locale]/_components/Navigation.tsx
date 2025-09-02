da049a04740cf54863e5260cb06b906a303f841a
Duane <ydgb0714@gmail.com>
2025-09-02 18:34:52 +0800

    Added link homepage

--- src/app/[locale]/_components/Navigation.tsx
+++ src/app/[locale]/_components/Navigation.tsx
@@ -41,7 +41,7 @@ export default function Navigation() {
         <header className="bg-white">
             <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
-                <a href="#" className="-m-1.5 p-1.5">
+                <a href="/" className="-m-1.5 p-1.5">
                     <span className="sr-only">elbayt.com</span>
                     <Image
                     alt=""
@@ -108,7 +108,7 @@ export default function Navigation() {
                 <div className="fixed inset-0 z-50" />
                 <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div className="flex items-center justify-between">
-                    <a href="#" className="-m-1.5 p-1.5">
+                    <a href="/" className="-m-1.5 p-1.5">
                     <span className="sr-only">elbayt.com</span>
                     <Image
                         alt=""
